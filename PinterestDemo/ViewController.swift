aa244d1d30fa468ebd29bdced7e20d56a578dc55
Trained-to-Glory <trainedtoglory@gmail.com>
2017-09-20 22:01:25 -0400

    Pinterest Commit

---
+++ PinterestDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PinterestDemo
+//
+//  Created by Jaylen Sanders on 9/20/17.
+//  Copyright © 2017 Glory. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
